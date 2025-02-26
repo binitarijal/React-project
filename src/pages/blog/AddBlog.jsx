ea28136845bd5f1f0a497b56aac95096a2cdd0aa
Binita rijal <rijalbinita01@gmail.com>
2025-02-26 17:16:11 +0545

    component layout

--- src/pages/blog/AddBlog.jsx
+++ src/pages/blog/AddBlog.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 
 const AddBlog = () => {
   return (
-    <div>AddBlog</div>
+    <>
+    <Navbar />
+    </>
   )
 }
 
---
+++ src/pages/blog/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+  <img class="w-full" src="https://r2.erweima.ai/imgcompressed/compressed_3cd28ff945d3870ce452fd77a10fd54c.webp" alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default Card
